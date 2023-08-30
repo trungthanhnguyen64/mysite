537f4ce2e2ecd6f9978211a7c08ad4dc9118a923
titvvn <trungnguyenthanh1901@gmail.com>
2023-08-30 14:58:49 +0700

    Part 7

--- catalog/models.py
+++ catalog/models.py
@@ -47,6 +47,12 @@ class Book(models.Model):
 
     display_genre.short_description = 'Genre'
 
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
--- catalog/views.py
+++ catalog/views.py
@@ -48,3 +48,27 @@ class AuthorDetailView(generic.DetailView):
 def author_detail_view(request, primary_key):
     author = get_object_or_404(Author, pk=primary_key)
     return render(request, 'catalog/auhtor_detail.html', context={'author': author})
+
+def index(request):
+    """View function for home page of site"""
+
+    # Generate counts of the main objects
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()# Available books (status = 'a')
+    num_authors = Author.objects.count()
+    
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
