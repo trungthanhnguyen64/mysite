7e7215d1a9251805ba1c1a8b48320e8303676ea8
trungthanhnguyen64 <88916714+trungthanhnguyen64@users.noreply.github.com>
2023-09-06 14:35:36 +0700

    Update catalog/tests/test_models.py
    Co-authored-by: huongnt-2545 <49477980+huongnt-2545@users.noreply.github.com>

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from catalog.models import Author
 
 class AuthorModelTest(TestCase):
-    classmethod
+    @classmethod
     def setUpTestData(cls): 
         # Set up non-modified objects used by all test methods
         Author.objects.create(first_name='Big', last_name='Bob')
